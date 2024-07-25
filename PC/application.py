6b15226ac85f82af98ab68a26852bec6cdee8093
Łukasz Rek <LRk@allseas.com>
2024-07-25 20:46:12 +0200

    Python rusts away

--- PC/application.py
+++
@@ -1,6 +0,0 @@
-from pycaw.pycaw import AudioUtilities, AudioDeviceState
-
-devices = AudioUtilities.GetAllDevices()
-for device in devices:
-    if device.state == AudioDeviceState.Active:
-        print(f"Name: {device.FriendlyName}")
